aea6c1d8b7e77fd5347d039c90533ec4fc31d4f8
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-06-01 12:29:20 -0400

    formatting landing and nav

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,16 +6,15 @@ function LandingPage() {
   return (
     <>
         <main id='main-landing'>
-            <h3>Plan your next trip with ease!</h3>
-            <h3>Search travel ideas</h3>
-            <h3>See what friends are up to</h3>
-            <h3>Save great recommendations</h3>
-            <p id='img'>placeholder for exciting travel img</p>
+            <h5 className='landing-main-text'>Plan your next trip with ease!</h5>
+            <br />
+            <h5 className='landing-main-text'>Search travel ideas</h5>
+            <h5 className='landing-main-text'>See what friends are up to</h5>
+            <h5 className='landing-main-text'>Save great recommendations</h5>
         </main>
-        <section id='start'>
-          <h4>Let's Go!</h4>
-          <Link to={'/search'}>Search</Link>
-          <Link to={'/new-trip'}>Plan</Link>
+        <section id='landing-start'>
+          <Link className='landing-links' to={'/search'}>Search</Link>
+          <Link className='landing-links' to={'/new-trip'}>Plan</Link>
         </section>
     </>
   );
