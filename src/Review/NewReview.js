7e914d8470b4352ff600ab38d596b6211c1e26a6
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-05-31 21:26:34 -0400

    review formatting

--- src/Review/NewReview.js
+++ src/Review/NewReview.js
@@ -5,32 +5,44 @@ function NewReview () {
     return (
         <section id='main-review'>
             <div id='container'>       
-                <h3>Review</h3>
+                <h3 id='review-title'>Review</h3>
                 <form>
-                    <input type='text' placeholder='name' required />
-                    <input type='text' placeholder='rating out of 5 stars' required />
-                    <input type='text' placeholder='location' required />
-                    <select required>
-                        <option>restaurant</option>
-                        <option>bar/coffee</option>
-                        <option>shopping</option>
-                        <option>activity</option>  
-                    </select>
-
-                    <span>
-                        <p>Pros</p>
-                        <textarea rows='5' required ></textarea>
-                    </span>
-                    <span>
-                        <p>Cons</p>
-                        <textarea rows='5' required ></textarea>
-                    </span>
+                    <h6>Name:</h6>
+                    <input className='allReview' type='text' placeholder='name' required />
                     
-                    <button id='search-btn'>Submit</button>
+                    <h6>Rating:</h6>
+                    <select className='allReview' required>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>  
+                    </select>
+                    
+                    <h6>Location:</h6>
+                    <div className='allReview'>
+                        <input type='text' placeholder='City' required />
+                        <input type='text' placeholder='Country' required />
+                    </div>
+                    
+                    <div>
+                        <h6>Category:</h6>
+                        <select className='allReview' required>
+                            <option>restaurant</option>
+                            <option>bar/coffee</option>
+                            <option>shopping</option>
+                            <option>activity</option>  
+                        </select>
+                    </div>
+
+                    <br />
+                    <h6>Comments:</h6>
+                    <textarea className='allReview' rows='5' required ></textarea>
+                    
+                    <button className='allReview' id='search-btn'>Submit</button>
                 </form>
             </div>
         </section>
-
     )
 }
 
