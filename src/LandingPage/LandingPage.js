6131e8a49c8bce467ea11880b5d8812921bee648
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-06-18 16:46:13 -0400

    added favicon

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -9,8 +9,8 @@ function LandingPage() {
             <h5 className='landing-main-text'>Plan your next trip with ease!</h5>
             <br />
             <h5 className='landing-main-text'>Search travel ideas</h5>
-            <h5 className='landing-main-text'>See what friends are up to</h5>
-            <h5 className='landing-main-text'>Save great recommendations</h5>
+            <h5 className='landing-main-text'>Create new trips</h5>
+            <h5 className='landing-main-text'>Post reviews of what you did</h5>
         </main>
         <section id='landing-start'>
           <Link className='landing-links' to={'/search'}>Search</Link>
