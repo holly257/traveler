245dc53b96926c31681ba7d45e9315ea3a02ccab
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-06-29 13:36:43 -0400

    pulled routes into own file

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppContext from './AppContext';
 import { API_URL } from '../config';
-import PrivateRoute from '../SignIn/AuthRouting/PrivateRoute';
-import Nav from '../Nav/Nav';
-import LandingPage from '../LandingPage/LandingPage';
-import SearchPage from '../SearchPage/SearchPage';
-import NewReview from '../Review/NewReview';
-import ReviewList from '../Review/ReviewList/ReviewList';
-import ReviewDetails from '../Review/ReviewList/ReviewDetails/ReviewDetails';
-import TripList from '../Trip/TripList/TripList';
-import NewTrip from '../Trip/NewTrip/NewTrip';
-import LogInPage from '../SignIn/LogIn/LogInPage';
-import SignUp from '../SignIn/SignUp';
-import NotFound from '../NotFound/NotFound';
+import AppRoute from '../AppRoute/AppRoute'
 
 class App extends React.Component {
     state = {
@@ -217,41 +205,7 @@ class App extends React.Component {
         };
         return (
             <AppContext.Provider value={value}>
-                <div className="App">
-                    <Nav />
-                    <Switch>
-                        <Route exact path="/" component={LandingPage} />
-                        <Route exact path="/search" component={SearchPage} />
-
-                        <Route exact path="/login" component={LogInPage} />
-                        <Route exact path="/signup" component={SignUp} />
-
-                        <PrivateRoute
-                            exact
-                            path="/review"
-                            component={ReviewList}
-                        />
-                        <PrivateRoute
-                            exact
-                            path="/review/:reviewId"
-                            component={ReviewDetails}
-                        />
-                        <PrivateRoute
-                            exact
-                            path="/new-review"
-                            component={NewReview}
-                        />
-
-                        <PrivateRoute
-                            exact
-                            path="/new-trip"
-                            component={NewTrip}
-                        />
-                        <PrivateRoute path="/trip" component={TripList} />
-
-                        <Route component={NotFound} />
-                    </Switch>
-                </div>
+                <AppRoute />
             </AppContext.Provider>
         );
     }
---
+++ src/AppRoute/AppRoute.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from '../SignIn/AuthRouting/PrivateRoute';
+import Nav from '../Nav/Nav';
+import LandingPage from '../LandingPage/LandingPage';
+import SearchPage from '../SearchPage/SearchPage';
+import NewReview from '../Review/NewReview';
+import ReviewList from '../Review/ReviewList/ReviewList';
+import ReviewDetails from '../Review/ReviewList/ReviewDetails/ReviewDetails';
+import TripList from '../Trip/TripList/TripList';
+import NewTrip from '../Trip/NewTrip/NewTrip';
+import LogInPage from '../SignIn/LogIn/LogInPage';
+import SignUp from '../SignIn/SignUp';
+import NotFound from '../NotFound/NotFound';
+
+class AppRoute extends React.Component {
+    render(){
+        return (
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/search" component={SearchPage} />
+
+                    <Route exact path="/login" component={LogInPage} />
+                    <Route exact path="/signup" component={SignUp} />
+
+                    <PrivateRoute
+                        exact
+                        path="/review"
+                        component={ReviewList}
+                    />
+                    <PrivateRoute
+                        exact
+                        path="/review/:reviewId"
+                        component={ReviewDetails}
+                    />
+                    <PrivateRoute
+                        exact
+                        path="/new-review"
+                        component={NewReview}
+                    />
+
+                    <PrivateRoute
+                        exact
+                        path="/new-trip"
+                        component={NewTrip}
+                    />
+                    <PrivateRoute path="/trip" component={TripList} />
+
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        )
+    }
+}
+
+export default AppRoute;
